eed1fd996550d5dedc905d70678a09f297cde39b
Kristian Snyder <knsnyder96@gmail.com>
2018-02-15 19:05:59 -0500

    Added custom port usage

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	SetUpAPICalls()
-
+	Conf, _ = ConfigRead("config.json")
 	svr := http.Server{
-		Addr:           ":8080",
+		Addr:           Conf.HTTPPort,
 		ReadTimeout:    5 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 8175, // if it's good enough for Apache, it's good enough for me
 	}
-	fmt.Println("Serving...")
+	fmt.Println("Serving on", Conf.HTTPPort+"...")
 	svr.ListenAndServe()
 }
